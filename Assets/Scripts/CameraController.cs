cd49703087922215e6402f69d4783cb756f16f3a
Tpbezerra <81422026+Tpbezerra@users.noreply.github.com>
2021-04-07 20:58:57 +0200

    Hoppas Git inte förstör detta

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -39,7 +39,7 @@ public class CameraController : MonoBehaviour
             zOffset = hit.distance;
 
         transform.localPosition = new Vector3(offset.x, 0, -zOffset);
-        
+
         Vector3 targetForward = transform.forward;
         targetForward.y = 0;
         targetForward.Normalize();
@@ -67,7 +67,7 @@ public class CameraController : MonoBehaviour
         target.targetedInteraction = null;
 
         RaycastHit hit;
-        if (Physics.SphereCast(transform.position, 0.2f, transform.forward, out hit, target.pickupDistance + offset.z, notPlayer))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, target.pickupDistance + offset.z, notPlayer))
         {
             Interactable interaction = hit.transform.GetComponent<Interactable>();
             if (interaction == null && hit.transform.parent != null)

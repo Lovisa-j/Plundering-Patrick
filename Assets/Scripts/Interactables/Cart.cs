67d495a32c48a6d220daa242cf54418f0ff403ca
Lovisa-j <80505874+Lovisa-j@users.noreply.github.com>
2021-06-06 19:15:04 +0200

    street level update
    added an animation + script, finished level

---
+++ Assets/Scripts/Interactables/Cart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Cart : MonoBehaviour
+{
+    [SerializeField] Animator animController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animController.SetBool("fallen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animController.SetBool("fallen", false);
+    }
+}
